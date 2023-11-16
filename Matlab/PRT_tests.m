518cb7b0d0c54e8b28103118af1e6a8eee604797
Ruppppp <rupprecht.martin67@gmail.com>
2023-11-16 13:16:37 +0100

    16/11 - 13:16
    Mise a jour code Matlab (affichage des contraintes)

--- Matlab/PRT_tests.m
+++ Matlab/PRT_tests.m
@@ -1,14 +1,15 @@
 clf;
 
-L = [25e-3 56e-3 20e-3 65e-3 9.8e-3];
-B = [50e-3 60e-3];
+L = [25e-3 56e-3 20e-3 65e-3 9.8e-3 50e-3 60e-3];
 
-th3 = 10*pi/180
+th3 = 10*pi/180;
 
-[d1, th2] = MGI(th3, L, B);
+[d1, th2] = MGI(th3, L);
 
-[P, Rc, Ro, F] = contraintes(d1, th2, th3, L, 15);
+Ch = 15;
 
-[th21, th22, th31, th32] = MGD(d1, L, B)
+[P, Rc, Ro, F] = contraintes(d1, th2, th3, L, Ch);
 
-drawArm(d1, th21, th31, L, B);
+[th21, th22, th31, th32] = MGD(d1, L);
+
+drawArm(d1, th21, th31, L, Ch);
--- Matlab/drawArm.m
+++ Matlab/drawArm.m
@@ -1,28 +1,48 @@
-function drawArm(d1, th2, th3, L, B)
+function drawArm(d1, th2, th3, L, Ch)
 
+% Definition parametres
 L1 = L(1);
-L2 = L(2);
 L3 = L(3);
 L4 = L(4);
 L5 = L(5);
+b1 = L(6);
+b2 = L(7);
 
-b1 = B(1);
-b2 = B(2);
+% Position des points
+O = [b1 0];
+C = [0 -b2];
+A = O + [0 -d1];
+B = A + [L1*sin(th2) -L1*cos(th2)];
+BC = C + [L3*sin(th3) L3*cos(th3)];
+CE = C + [-L4*sin(th3) -L4*cos(th3)];
+E = CE + [-L5*cos(th3) L5*sin(th3)];
 
-line([0 b1],[0 0],'color','r')
-line([0 0],[0 -b2],'color','r')
+% Lignes entre les points
+line([0 O(1)],[0 O(2)],'color','cyan')
+line([0 C(1)],[0 C(2)],'color','cyan')
+line([O(1) A(1)],[O(2) A(2)],'color','b')
+line([A(1) B(1)],[A(2) B(2)],'color','g')
+line([C(1) BC(1)],[C(2) BC(2)],'color','black')
+line([BC(1) B(1)],[BC(2) B(2)],'color','black')
+line([C(1) CE(1)],[C(2) CE(2)],'color','black')
+line([CE(1) E(1)],[CE(2) E(2)],'color','black')
 
-line([b1 b1],[0 -d1],'color','b')
+% Affichage des contraintes internes
+scale = .0005;
+[P, Rc, Ro, F] = contraintes(d1, th2, th3, L, Ch);
+ln = line([C(1) C(1)+Rc(3,1)*scale],[C(2) C(2)+Rc(3,1)*scale],'color','r');
+ln.LineWidth = 3;
+ln = line([O(1) O(1)+Ro(1,1)*scale],[O(2) O(2)+Ro(3,1)*scale],'color','r');
+ln.LineWidth = 3;
 
-line([b1 b1+L1*sin(th2)],[-d1 -d1-L1*cos(th2)],'color','g')
 
-line([0 L3*sin(th3)],[-b2 -b2+L3*cos(th3)],'color','black')
-line([ L3*sin(th3) L3*sin(th3)+L2*cos(th3)],[-b2+L3*cos(th3) -b2+L3*cos(th3)-L2*sin(th3)],'color','black')
-line([0 -L4*sin(th3)],[-b2 -b2-L4*cos(th3)],'color','black')
-line([-L4*sin(th3) -L4*sin(th3)-L5*cos(th3)],[-b2-L4*cos(th3) -b2-L4*cos(th3)+L5*sin(th3)],'color','black')
 
-axis([-L4 b1+L1 -b2-L4-.01 .01 ]) % Set the axis limits
 
+
+
+
+% Definition des axes
+axis([-L4 b1+L1 -b2-L4-.01 .01 ])
 set(gca,'DataAspectRatio',[1,1,1])
 
 end
