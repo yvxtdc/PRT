cdd0258d8b66255442832f75e5b7b639ff24dc9e
Ruppppp <rupprecht.martin67@gmail.com>
2023-12-07 13:58:36 +0100

    07/12 - 13:58
    Optimisation raideur col

--- Matlab/optimisationCol.m
+++ Matlab/optimisationCol.m
@@ -1,3 +1,5 @@
-function r = optimisationCol(Re, E, ksec, e, b)
+function r = optimisationCol(Re, E, ksec, e, b, th)
 
-r = (16*E^2*b^2*e^3*ksec^2*theta^2)/(9*Pi^2*(Re^2*b^2*e^2 + 2*F*Re*b*e*ksec + F^2*ksec^2));
+% Calcule le rayon r en fonction des autres paramètres afin de respecter le
+% coef de sécurité
+r = (16*E^2*b^2*e^3*ksec^2*th^2)/(9*Pi^2*(Re^2*b^2*e^2 + 2*F*Re*b*e*ksec + F^2*ksec^2));
