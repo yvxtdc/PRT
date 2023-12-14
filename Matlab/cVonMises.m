a90a711f0ef87318a292642fe7289621cc30f4df
yvxtdc <97665169+yvxtdc@users.noreply.github.com>
2023-12-14 11:17:17 +0100

    14-12-23-2

--- Matlab/cVonMises.m
+++ Matlab/cVonMises.m
@@ -2,23 +2,23 @@ function VonMises=cVonMises(materiau, type, t, r, b, theta, L, F)
 
 if strcmp(materiau, 'aluminium')
     E = 70000;
-    disp('Materiau choisi: aluminium.');
+    %disp('Materiau choisi: aluminium.');
 elseif strcmp(materiau, 'acier')
     E = 210000;
-    disp('Materiau choisi: acier.');
+    %disp('Materiau choisi: acier.');
 elseif strcmp(materiau, 'PLA')
     E = 3000;
-    disp('Materiau choisi: PLA.');
+    %disp('Materiau choisi: PLA.');
 elseif strcmp(materiau, 'ABS')
     E = 2000;
-    disp('Materiau choisi: ABS.');
+    %disp('Materiau choisi: ABS.');
 end
 
-if strcmp(type, 'col')
-    disp('Liaison choisie: col.');
-elseif strcmp(type, 'lames')
-    disp('Liaison choisie: lames.');
-end
+% if strcmp(type, 'col')
+%     disp('Liaison choisie: col.');
+% elseif strcmp(type, 'lames')
+%     disp('Liaison choisie: lames.');
+% end
 
 Cf = cf(type, E, r ,t, theta, L);
 Cc = cc(type, F, b, t);
