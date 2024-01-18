be7cb95075041171a89e4001fdc9e9c0d354f37f
Ruppppp <rupprecht.martin67@gmail.com>
2024-01-18 16:19:31 +0100

    18/01 - 16:19
    Commentaires

--- Matlab/optimisationCol.m
+++ Matlab/optimisationCol.m
@@ -1,5 +1,5 @@
 function r = optimisationCol(Re, E, ksec, e, b, th,F)
+% Calcule le rayon r de la liaison a cols circulaire en fonction des autres paramètres afin de respecter le
+% coefficient de sécurité (toutes les dimensions des cols en mm)
 
-% Calcule le rayon r en fonction des autres paramètres afin de respecter le
-% coef de sécurité
 r = (e.^3.*(16*E^2*b^2*ksec^2*th^2))./(9*pi^2*(Re^2*b^2*e.^2 - e.*2*F*Re*b*ksec + F^2*ksec^2));
--- Matlab/optimisationRaideurCol.m
+++ Matlab/optimisationRaideurCol.m
@@ -1,4 +1,7 @@
 function [K, r] = optimisationRaideurCol(Re, E, ksec, e, b, th)
+% Donne la raideur du col optimise afin de respecter le coefficient de
+% securite pour les dimensions e et b (en mm) donnees. Le rayon des cols
+% est egalement calcule
 
 r = optimisationCol(Re, E, ksec, e, b, th,25);
 K = (e.^(2.5).*(2*E*b))./(sqrt(r).*(9*pi));
